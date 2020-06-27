d531483fd6a33387ac7dc7915a06da310a00f9ca
John Saputo <jsaputo1@gmail.com>
2020-06-27 18:55:11 +0000

    added login route and index route

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,102 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
 module.exports = (db) => {
+  /*
+
+
+  Routes Below
+
+
+  */
+
+  /*
+  Index Routes
+  */
+
+  // Index (get all listings)
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+    database
+      .getAllListings(req.query)
+      .then((listings) => res.send({ listings }))
+      .catch((e) => {
+        console.error(e);
+        res.send(e);
       });
   });
+
+  //Post route to filter by price
+
+  router.post("/", (req, res) => {
+    database
+      .getAllListingsByPrice(req.query)
+      .then((listings) => res.send({ listings }))
+      .catch((e) => {
+        console.error(e);
+        res.send(e);
+      });
+  });
+
+  // //Post request to filter by price
+  // app.post("/:user", (req, res) => {
+  //   const queryString = `
+  //   query to sort products by price
+  //   `;
+  //   pool
+  //     .query(queryString)
+  //     .then((res) => res.rows)
+  //     .then((products) => {
+  //       res.render("index");
+  //       console.log("Post request to filter items by price");
+  //     });
+  // });
+
+  //Login Routes
+  /**
+   * Check if a user exists with a given username and password
+   * @param {String} email
+   * @param {String} password encrypted
+   */
+  const login = function (email, password) {
+    return database.getUserWithEmail(email).then((user) => {
+      if (bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    });
+  };
+  exports.login = login;
+
+  router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    login(email, password)
+      .then((user) => {
+        if (!user) {
+          res.send({ error: "error" });
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({ user: { name: user.name, email: user.email, id: user.id } });
+      })
+      .catch((e) => res.send(e));
+  });
+
+  // Logout Post
+
+  router.post("/logout", (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
+
+  /*
+
+    End of routes
+
+
+    */
+
   return router;
 };
