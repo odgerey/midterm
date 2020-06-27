73b2dd06f1dababdb6cf5efbfb5116b68f0c7669
John Saputo <jsaputo1@gmail.com>
2020-06-27 20:53:30 +0000

    created post route for login

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const { user } = require("osenv");
+const { redirect } = require("statuses");
 module.exports = (db) => {
   /*
 
@@ -22,117 +23,103 @@ module.exports = (db) => {
   Index Routes
   */
 
-
- router.get("/", (req, res) => {
-  db.query(`
-
-  query to get all of the listings
-
-
-  `)
-    .then(data => {
-      const users = data.rows;
-      res.json({ users });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-
+  router.get("/", (req, res) => {
+    db.query(
+      `
+      SELECT * FROM listings
+  `
+    )
+      .then((data) => {
+        const users = data.rows;
+        res.json({ users });
+        res.render("index");
+        console.log("test");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
   //Post route to filter by price
 
   router.post("/:price", (req, res) => {
-    db.query(`
-    query to get afilter the listings by price
-
-    `)
-      .then(data => {
+    db.query(
+      `
+      SELECT price FROM listings
+      `
+    )
+      .then((data) => {
         const products = data.rows;
         res.json({ products });
+        res.render("index");
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-
-
-  //Login Routes
-  /**
-   * Check if a user exists with a given username and password
-   * @param {String} email
-   * @param {String} password encrypted
-   */
-
-   //helper function
-  const login = function (email) {
-
-    //user check
-
-    return email
-
-  };
-
-
-//login get route
-
-  router.post("/login", (req, res) => {
-    const { email } = req.body;
-    login(email)
-      .then((email) => {
-        if (!email) {
-          res.send({ error: "error" });
-          return;
-        }
-        req.session.email = email;
-      })
-      .catch((e) => res.send(e));
-  });
-
-  // Logout Post
-
-  router.post("/logout", (req, res) => {
-    req.session.userId = null;
-    res.send({});
-  });
-
-  /*
-    User Specific Routes
-  */
-
-  //Get user page
-  router.get("/:user", (req, res) => {
-    database
-      .getAllFavorites(req.query)
-      .then((listings) => res.send({ listings }))
-      .catch((e) => {
-        console.error(e);
-        res.send(e);
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  // //Get user page
-  // app.get("/:user", (req, res) => {
-  //   const queryString = `
-  //   query to pull all user's favourite products
-  //   `;
-  //   pool
-  //     .query(queryString)
-  //     .then((res) => res.rows)
-  //     .then((products) => {
-  //       res.render("user_page");
-  //       console.log("Get request for individual user page");
-  //     });
-  // });
+    // Helper function to check username
+    const login = function (email)
+    };
 
-  /*
+  //login get route
 
-    End of routes
+    router.post("/login", (req, res) => {
+      const { email } = req.body;
+      login(email)
+        .then((email) => {
+          if (!email) {
+            res.send({ error: "error" });
+            return;
+          }
+          req.session.email = email;
+          res.redirect("/")
 
+        })
+        .catch((e) => res.send(e));
+    });
 
-    */
+  //   // Logout Post
+
+  //   router.post("/logout", (req, res) => {
+  //     req.session.email = null;
+  //   //redirect
+  //   });
+
+  //   /*
+  //     User Specific Routes
+  //   */
+
+  //   //Get user profile page
+  //   router.get("/:user", (req, res) => {
+  //     database
+  //       .getAllFavorites(req.query)
+  //       .then((listings) => res.send({ listings }))
+  //       .catch((e) => {
+  //         console.error(e);
+  //         res.send(e);
+  //       });
+  //   });
+
+  //   // //Get user page
+  //   // app.get("/:user", (req, res) => {
+  //   //   const queryString = `
+  //   //   query to pull all user's favourite products
+  //   //   `;
+  //   //   pool
+  //   //     .query(queryString)
+  //   //     .then((res) => res.rows)
+  //   //     .then((products) => {
+  //   //       res.render("user_page");
+  //   //       console.log("Get request for individual user page");
+  //   //     });
+  //   // });
+
+  //   /*
+
+  //     End of routes
+
+  //     */
 
   return router;
 };
