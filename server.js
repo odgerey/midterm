0d85b8901e896698af7698db2149f644d6d55f6e
Dmitry Petrov <e.liberate@gmail.com>
2020-06-28 21:30:18 +0000

    Added placeholder for the page to send a message to the seller

--- server.js
+++ server.js
@@ -93,6 +93,11 @@ app.get("/listings/new", (req, res) => {
   console.log("GET request for a new listing page");
 });
 
+app.get("/send_message", (req, res) => {
+  res.render("send_message.ejs");
+  console.log("GET request for a page to send a message to the seller");
+});
+
 // app.post("/login", (req, res) => {
 //   console.log("POST request for Login");
 // });
