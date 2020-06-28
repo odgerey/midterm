c579d47e0e59fa7fa29fd844d73ce030bf3bef33
John Saputo <jsaputo1@gmail.com>
2020-06-28 15:03:56 +0000

    commented out code for testing

--- server.js
+++ server.js
@@ -61,15 +61,15 @@ app.use(
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get("/login", (req, res) => {
-  res.render("login");
-  console.log("Get request for login page");
-});
+// app.get("/login", (req, res) => {
+//   res.render("login");
+//   console.log("Get request for login page");
+// });
 
-app.get("/listings", (req, res) => {
-  res.render("listings");
-  console.log("Get request for listings page");
-});
+// app.get("/listings", (req, res) => {
+//   res.render("listings");
+//   console.log("Get request for listings page");
+// });
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
