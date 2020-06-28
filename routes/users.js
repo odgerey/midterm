d237f3e56cd7b4289f82dcd64ebd1aa4ff4cbe04
John Saputo <jsaputo1@gmail.com>
2020-06-28 01:07:49 +0000

    refactored code

--- routes/users.js
+++ routes/users.js
@@ -6,177 +6,173 @@
  */
 
 const express = require("express");
-const router = express.Router();
-const bcrypt = require("bcrypt");
-const { user } = require("osenv");
-const { redirect } = require("statuses");
+const { c } = require("tar");
 const { query } = require("express");
+const router = express.Router();
+// const bcrypt = require("bcrypt");
+// const { user } = require("osenv");
+// const { redirect } = require("statuses");
+// const { query } = require("express");
+
 module.exports = (db) => {
   /*  Index Routes  */
 
   //GET route to show index. Index displays all listings.
   router.get("/", (req, res) => {
-    console.log("Working");
     const queryString = `
     SELECT *
     FROM listings;
-`;
+    `;
     db.query(queryString)
       .then((data) => {
-        // const products = data.rows;
-        // const templateVars = {};
+        const products = data.rows;
+        const templateVars = {};
+        console.log(products);
         console.log("GET request for index page");
-        // res.render("index", templateVars);
+        res.render("index", templateVars);
       })
       .catch((err) => {
-        console.log("catch");
-
         res.status(500).json({ error: err.message });
       });
   });
 
-  // //POST route to filter by price
-  // router.post("/:price", (req, res) => {
-  //   db.query(
-  //     `
-  //       SELECT price FROM listings
-  //       `
-  //   )
-  //     .then((data) => {
-  //       const products = data.rows;
-  //       res.render("index");
-  //       console.log("Post request for filter by price");
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({ error: err.message });
-  //     });
-  // });
+  //POST route to filter by price
+  router.post("/:price", (req, res) => {
+    const queryString = `query to sort products by price`;
+    db.query(queryString)
+      .then((data) => {
+        const products = data.rows;
+        console.log(products);
+        res.render("index");
+        console.log("POST request for filter by price");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
   // /* Login Routes */
 
-  // // Helper function to check username
-  // const loginVerification = function (email) {
-  //   const emailFromDatabase = "";
-  //   if (email === emailFromDatabase) {
-  //     return true;
-  //   }
-  //   false;
-  // };
+  //GET request for login page
+  router.get("login", (req, res) => {
+    console.log("GET request for login page");
+    res.render("login");
+  });
 
-  // //POST route to login. Stores e-mail address in cookie
-  // router.post("/login", (req, res) => {
-  //   const email = req.body.email;
-  //   loginVerification(email)
-  //     .then((email) => {
-  //       if (!email) {
-  //         console.log("Post login: Login credential error");
-  //         res.send({ error: "error" });
-  //         return;
-  //       }
-  //       email = req.session.email;
-  //       console.log("Post login: Success");
-  //       res.redirect("/");
-  //     })
-  //     .catch((e) => res.send(e));
-  // });
+  // Helper function to check username
+  const loginVerification = function (email) {
+    const emailFromDatabase = "";
+    if (email === emailFromDatabase) {
+      return true;
+    }
+    return false;
+  };
 
-  // // POST route to logout. Sets cookie to NULL
-  // router.post("/logout", (req, res) => {
-  //   const userCookie = req.session.email;
-  //   userCookie = null;
-  //   res.redirect("/login");
-  // });
+  //POST route to login. Stores e-mail address in cookie
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+    console.log("Email should be value of form: ");
+    loginVerification(email)
+      .then((email) => {
+        if (!email) {
+          console.log("Post login: Login credential error");
+          res.send({ error: "error" });
+          return;
+        }
+        email = req.session.email;
+        console.log("Email should be cookie: ");
+        console.log("Post login: Success");
+        res.redirect("/");
+      })
+      .catch((e) => res.send(e));
+  });
 
-  // /* User Specific Routes */
+  // POST route to logout. Sets cookie to NULL
+  router.post("/logout", (req, res) => {
+    const userCookie = req.session.email;
+    userCookie = null;
+    res.redirect("/login");
+  });
 
-  // //GET route for buyer's page. Shows all favourite items.
-  // router.get("/:user", (req, res) => {
-  //   db.query(
-  //     `
-  //     query to show all user's favourites
+  /* User Specific Routes */
 
-  //     `
-  //   )
-  //     .then((data) => {
-  //       const products = data.rows;
-  //       const templateVars = {};
-  //       console.log("Get request for user page");
-  //       res.render("user", templateVars);
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({ error: err.message });
-  //     });
-  // });
+  //GET route for buyer's page. Shows all favourite items.
+  router.get("/:user", (req, res) => {
+    const queryString = `   `;
+    db.query(queryString)
+      .then((data) => {
+        const products = data.rows;
+        const templateVars = {};
+        console.log(products);
+        console.log("Get request for buyer page");
+        res.render("user", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-  // //POST route to add favourite
-  // router.post("/:price", (req, res) => {
-  //   db.query(
-  //     `
-  //     Query to add favourite
-  //     `
-  //   )
-  //     .then((data) => {
-  //       const products = data.rows;
-  //       const templateVars = {};
-  //       console.log("POST request to add favourite");
-  //       res.render("index", templateVars);
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({ error: err.message });
-  //     });
-  // });
+  //POST route to add favourite
+  router.post("/:price", (req, res) => {
+    const queryString = ` Query to add favourite  `;
+    db.query(queryString)
+      .then((data) => {
+        const products = data.rows;
+        const templateVars = {};
+        console.log(products);
+        console.log("POST request to add favourite");
+        res.render("index", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-  // //GET route to view seller's listings
-  // router.get("/listings:user", (req, res) => {
-  //   db.query(
-  //     `
-  //     Query to view specific seller's items
-  //     `
-  //   )
-  //     .then((data) => {
-  //       const products = data.rows;
-  //       const templateVars = {};
-  //       console.log("GET request to view seller's listings");
-  //       res.render("user-listings", templateVars);
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({ error: err.message });
-  //     });
-  // });
+  //GET route to view seller's listings
+  router.get("/listings:user", (req, res) => {
+    const queryString = ` Query to add specific sellers items  `;
+    db.query(queryString)
+      .then((data) => {
+        const products = data.rows;
+        const templateVars = {};
+        console.log(products);
+        console.log("GET request to view seller's listings");
+        res.render("user-listings", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-  // //POST route to edit seller's listings
-  // router.post("/listings:user", (req, res) => {
-  //   db.query(
-  //     `
-  //     Query to edit items
-  //     `
-  //   )
-  //     .then((data) => {
-  //       const products = data.rows;
-  //       console.log("POST request to edit items");
-  //       res.render("user-listings");
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({ error: err.message });
-  //     });
-  // });
+  //POST route to edit seller's listings
+  router.post("/listings:user", (req, res) => {
+    const queryString = ` query to edit items`;
+    db.query(queryString)
+      .then((data) => {
+        const products = data.rows;
+        console.log(products);
+        console.log("POST request to edit items");
+        res.render("user-listings");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-  // //POST route to delete seller's listings
-  // router.post("/listings:user/delete", (req, res) => {
-  //   db.query(
-  //     `
-  //     Query to delete items
-  //     `
-  //   )
-  //     .then((data) => {
-  //       const products = data.rows;
-  //       console.log("POST request to delete items");
-  //       res.render("user-listings");
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({ error: err.message });
-  //     });
-  // });
+  //POST route to delete seller's listings
+  router.post("/listings:user/delete", (req, res) => {
+    const queryString = ` Query to delete items `;
+    db.query(queryString)
+      .then((data) => {
+        const products = data.rows;
+        console.log(products);
+        console.log("POST request to delete items");
+        res.render("user-listings");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
   // /* End of Routes */
 
