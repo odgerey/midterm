e6691d50f7a900080b0dfd377eb311849ad02a52
Dmitry Petrov <e.liberate@gmail.com>
2020-06-29 18:58:50 +0000

    Updated the header and added a button to go to the user page

--- scripts/client.js
+++ scripts/client.js
@@ -14,5 +14,7 @@ $('document').ready(function() {
     $('#received-messages').slideToggle('fast');
   });
 
-
+  $('#my-account-button').on('click', function() {
+    location.href = "/users/:id";
+  });
 });
