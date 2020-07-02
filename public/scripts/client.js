eec3f0803fa8fab3f5944e4ecb4a7a9b362163e9
Dmitry Petrov <e.liberate@gmail.com>
2020-07-02 15:26:06 +0000

    Added an admin page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -66,4 +66,35 @@ $(document).ready(function() {
     alert('Your message has been sent!');
   });
 
+  // Checking and showing an error if the fields are empty when submitting forms on add or edit listing pages
+
+  $('form').on('submit', function(event) {
+
+    event.preventDefault();
+
+    let tweetText = $('#tweet-text').val();
+
+    if (tweetText.length > 140) {
+      $('.error').slideUp('fast');
+      $('.error').html('The tweet exceeds the maximum length');
+      $('.error').slideDown('fast');
+    } else if (!tweetText) {
+      $('.error').slideUp('fast');
+      $('.error').html('Please enter the tweet');
+      $('.error').slideDown('fast');
+    } else {
+      $('.error').slideUp('fast');
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: $(this).serialize(),
+      })
+        .then($('#tweets-container').empty())
+        .then($('#tweet-text').val(""))
+        .then($('output').val(140))
+        .then(loadTweets);
+    }
+
+  });
+
 })
