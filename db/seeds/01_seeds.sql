78d855c3487c396def9be7723b2b8e52fe8074d8
John Saputo <jsaputo1@gmail.com>
2020-07-02 18:10:37 +0000

    changed images in listings to use local path

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -20,24 +20,24 @@ VALUES (1),
 (4);
 
 INSERT INTO listings(title, description, thumbnail_photo_url, cover_photo_url, created_at, price, for_sale, seller_id)
-VALUES ('Norman Guitar', 'Brand new, barely used','https://i.imgur.com/OO1D0ty.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z', 300, true, 1 ),
+VALUES ('Norman Guitar', 'Brand new, barely used','/images/guitar.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z', 300, true, 1 ),
 
-('Glitter Box', 'Crazy lady selling box of glitter','https://i.imgur.com/BbA3TvF.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z', 45,true, 2 ),
+('Glitter Box', 'Crazy lady selling box of glitter','/images/glitter.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z', 45,true, 2 ),
 
 ('The Cranberries RARE autographed copy', 'CD signed by Dolores O Riodan, collectors item','https://i.imgur.com/VdV5qpE.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z',80, true, 3),
 
-('Seinfeld Seasons 1-6', 'Mint condition','https://i.imgur.com/lmEYNG9.jpg', 'CoverPhoroURL','2018-02-12T08:13:20.000Z',98, true, 4 ),
+('Seinfeld Seasons 1-6', 'Mint condition','/images/seinfeld.jpg', 'CoverPhoroURL','2018-02-12T08:13:20.000Z',98, true, 4 ),
 
-('Sofa to give away', 'Pick it up and it is yours','https://i.imgur.com/elgE9Vz.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z',0,true, 5 ),
+('Sofa to give away', 'Pick it up and it is yours','/images/sofa.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z',0,true, 5 ),
 
-('Marathon gear', 'A little sweaty, nothing a little cleaning can fix','https://i.imgur.com/cV5hRdH.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z', 100,true, 6 ),
+('Marathon gear', 'A little sweaty, nothing a little cleaning can fix','/images/marathon.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z', 100,true, 6 ),
 
-('Collection of Encyclopedias', 'The full collection A to Z dating form 1975, vintage!','https://i.imgur.com/tJVb8mH.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z', 75,true, 7 ),
+('Collection of Encyclopedias', 'The full collection A to Z dating form 1975, vintage!','/images/encyclopedia.jpg', 'CoverPhotoURL','2018-02-12T08:13:20.000Z', 75,true, 7 ),
 
 
-('Pumpkin', 'Just a pumpkin','https://i.imgur.com/GO396ow.jpg', 'CoverPhotoURL', '2018-02-12T08:13:20.000Z',10,true, 8),
+('Pumpkin', 'Just a pumpkin','/images/pumpkin.jpg', 'CoverPhotoURL', '2018-02-12T08:13:20.000Z',10,true, 8),
 
-('Rayban sunglasses 1995', 'Brand new, barely used','https://i.imgur.com/q3pFVCN.jpg', 'CoverPhotoURL', '2018-02-12T08:13:20.000Z', 225, true, 9 );
+('Rayban sunglasses 1995', 'Brand new, barely used','/images/sunglasses.jpg', 'CoverPhotoURL', '2018-02-12T08:13:20.000Z', 225, true, 9 );
 
 
 INSERT INTO favorites (buyer_id, listing_id)
