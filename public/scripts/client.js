8ea9397667af1b052dab347729c50f08300d3859
Dmitry Petrov <e.liberate@gmail.com>
2020-07-02 01:22:38 +0000

    Added the function to update favourites when removing a listing from favourites

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,6 +8,123 @@ $(document).ready(function() {
     //   return div.innerHTML;
     // };
 
+  // Creating a favorite listing HTML element
+
+  // const createFavorite = function(favorite) {
+  //   const createdFavorite = `
+  //     <div class="one-listing" id='${favorite.listing_id}'>
+  //     <div class="listing-info">
+  //       <img src="${favorite.thumbnail_photo_url}" />
+  //       <div>
+  //         <div class="listing-parameters">
+  //           <h3>${favorite.title}</h3>
+  //           <div>
+  //             <span>CAD ${favorite.price}</span>
+  //             <span>Posted on: ${favorite.date}</span>
+  //           </div>
+  //         </div>
+  //         <article class="listing-description">
+  //           ${favorite.description}
+  //         </article>
+  //       </div>
+  //     </div>
+  //     <div class="listing-buttons">
+  //       <form
+  //         method="POST"
+  //         action="/favorites/remove_favorite/${favorite.listing_id}"
+  //         class="remove-favorite-form"
+  //       >
+  //         <button type="submit" class="button-favorite">
+  //           <i class="fas fa-heart"></i>
+  //         </button>
+  //       </form>
+  //       <form
+  //         method="GET"
+  //         action="/messages/listings/${favorite.listing_id}/messages/"
+  //       >
+  //         <button type="submit" class="new-message-button">
+  //           Contact
+  //         </button>
+  //       </form>
+  //     </div>
+  //   </div>
+  //   <hr />
+  //   `;
+  //   return createdFavorite;
+  // };
+
+  // Adding the favourites to the existing base HTML
+
+  // const renderFavorites = function (favorites) {
+  //   for (let favorite of favorites) {
+  //     const currentFavorite = createFavorite(favorite);
+  //     $('#favorites-listings').append(currentFavorite);
+  //   }
+  // };
+
+  // const renderFavorites2 = function (favorites) {
+
+  //   const createFavorite = function(value) {
+  //     const createdFavorite = `
+  //       <div class="one-listing" id='${favorites[value].listing_id}'>
+  //       <div class="listing-info">
+  //         <img src="${favorites[value].thumbnail_photo_url}" />
+  //         <div>
+  //           <div class="listing-parameters">
+  //             <h3>${favorites[value].title}</h3>
+  //             <div>
+  //               <span>CAD ${favorites[value].price}</span>
+  //               <span>Posted on: ${favorites[value].date}</span>
+  //             </div>
+  //           </div>
+  //           <article class="listing-description">
+  //             ${favorites[value].description}
+  //           </article>
+  //         </div>
+  //       </div>
+  //       <div class="listing-buttons">
+  //         <form
+  //           method="POST"
+  //           action="/favorites/remove_favorite/${favorites[value].listing_id}"
+  //           class="remove-favorite-form"
+  //         >
+  //           <button type="submit" class="button-favorite">
+  //             <i class="fas fa-heart"></i>
+  //           </button>
+  //         </form>
+  //         <form
+  //           method="GET"
+  //           action="/messages/listings/${favorites[value].listing_id}/messages/"
+  //         >
+  //           <button type="submit" class="new-message-button">
+  //             Contact
+  //           </button>
+  //         </form>
+  //       </div>
+  //     </div>
+  //     <hr />
+  //     `;
+  //     return createdFavorite;
+  //   };
+
+  //   for (let favorite of favorites) {
+  //     const currentFavorite = createFavorite(favorite);
+  //     $('#favorites-listings').append(currentFavorite);
+  //   }
+  // };
+
+  // Loading the favourites HTML after removing a listing from favourites
+
+  // const loadFavorites = function() {
+  //   $.ajax({
+  //     url: '/myaccount',
+  //     method: 'GET'
+  //   })
+  //     .then(function(response) {
+  //       renderFavorites2(response);
+  //     });
+  // };
+
   // Showing or hiding the received messages on the messages page
   $('#show-messages-button').on('click', function() {
     $('.previous-message').slideToggle('fast');
@@ -63,6 +180,9 @@ $(document).ready(function() {
     })
       // .then(() => console.log(listingId))
       // .then(() => $(`#${listingId}`).hide('fast'));
+      // .then(() => $(currentListing).slideUp('fast'));
+      // .then($('#favorites-listings').empty())
+      // .then(loadFavorites)
   });
 
   // Showing an alert when a message is sent
