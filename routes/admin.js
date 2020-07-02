57d3ffedd525c6a23d3bc2abc9a1d64975f504d5
John Saputo <jsaputo1@gmail.com>
2020-07-02 18:37:11 +0000

    fixed admin route typo

---
+++ routes/admin.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  //GET route for buyer's page. Shows all favourite items.
+  router.get("/", (req, res) => {
+    console.log("test");
+    const favoritesQuery = `
+    SELECT listings.*, favorites.*
+    FROM favorites
+    JOIN listings ON favorites.listing_id = listings.id
+    JOIN buyers ON favorites.buyer_id = buyers.id
+    WHERE buyers.email = $1;
+    `;
+    const listingsQuery = `
+      SELECT *
+      FROM listings
+      WHERE seller_id = $1;
+      `;
+    const messagesQuery = `
+      SELECT *
+      FROM messages
+      WHERE buyer_id = $1
+      OR seller_id = $1;
+
+      `;
+    const email = req.session.email;
+    const username = email;
+    const promises = [
+      db.query(favoritesQuery, [email]),
+      db.query(listingsQuery, [req.session.buyer_id]),
+      db.query(messagesQuery, [req.session.buyer_id]),
+    ];
+
+    Promise.all(promises)
+      .then(([favoritesResults, listingResults, messagesResults]) => {
+        const favorites = favoritesResults.rows;
+        const listings = listingResults.rows;
+        const messages = messagesResults.rows;
+        const templateVars = { favorites, listings, messages, username };
+        res.render("admin", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  return router;
+};
