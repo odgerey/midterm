45fef5c4dbb591e64dd242eecf59130f84270497
John Saputo <jsaputo1@gmail.com>
2020-07-02 21:23:38 +0000

    moved isAdmin to helperFunction file

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,3 +1,30 @@
+const isAdmin = function (db, id, buyerID) {
+  const getListing = `
+    SELECT *
+    FROM listings
+    WHERE id = $1
+    `;
+  const getBuyer = `
+    SELECT *
+    FROM buyers
+    WHERE id = $1
+    `;
+  return Promise.all([
+    db.query(getListing, [id]),
+    db.query(getBuyer, [buyerID]),
+  ]).then(([listingResults, buyerResults]) => {
+    const buyer = buyerResults.rows[0];
+    const listing = listingResults.rows[0];
+    console.log("Buyer", buyer, "listing", listing);
+
+    if (buyer.is_admin || buyer.id === listing.seller_id) {
+      return listing;
+    } else {
+      return Promise.reject({ message: "not allowed" });
+    }
+  });
+};
+
 const ifLoggedIn = function (cookie) {
   if (cookie === null) {
     return false;
@@ -30,4 +57,5 @@ const logInMiddleware = function (req, res, next) {
 module.exports = {
   logInMiddleware,
   isFavorite,
+  isAdmin,
 };
